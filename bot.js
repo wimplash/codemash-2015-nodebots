ff216ad0c555682671f0c83c79951489e9867567
Ryan Ransford <rdransfo@gmail.com>
2015-01-06 09:16:10 -0500

    adding support for changing the LED brightness using the potentiometer

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
 var five = require('johnny-five');
 var board = new five.Board();
 board.on('ready', function() {
-	(new five.Led(11)).strobe();
+	var led = new five.Led(11);
 	var input = new five.Sensor('A0');
+
 	input.on('data', function() {
-		console.log(this.value);
+		setLed(this.value);
 	});
+
+	function setLed(value) {
+		var brightness = five.Fn.map(value, 0, 1025, 0, 255);
+		console.log('setting LED brightness to ' + brightness);
+		led.brightness(brightness);
+	}
 });
